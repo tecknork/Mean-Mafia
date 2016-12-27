8b55e94b82a176600f161bdef1715b081a1c09a8
Murtaza Raza <murtaza@mailcohen.com>
2016-12-27 07:39:23 +0300

    setup socket

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.get('/',function(req,res,next){
 
-    res.render('index');
+    res.render('index.html');
 
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var todos = require('./routes/todos');
+var socketer = require('./routes/socketer');
 
 var app = express();
 
@@ -12,7 +13,8 @@ var app = express();
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
-
+app.engine('html',require('ejs').renderFile);
+app.use(express.static(path.join(__dirname,'client')));
 
 
 app.use(bodyParser.json());
@@ -21,7 +23,7 @@ app.use(bodyParser.urlencoded( { extended:false }));
 
 app.use('/',index);
 app.use('/api/v1/',todos);
-
+app.use('/sockets/',socketer);
 
 app.listen(3000,function(){
         console.log('server started on port 3000');
